61c0b132aec4c0a0e501e5a5550241baa1c306c2
Antoine Rousseau <_antoine_@metalu.net>
2018-12-06 19:09:30 +0100

    first Baguettes commit

---
+++ Baguettes/fwAxl/config.h
@@ -0,0 +1,22 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define ANALOG_FILTER 4 
+#define ANALOG_THRESHOLD 10
+
+#define I2CMASTER_PORT 1
+
+#define SWITCH K11
+#define SWLED K12
+
+#define PEDAL1 MB1
+#define PEDAL2 MBEN
+#define PEDAL3 MBPWM
+#define PEDAL4 MOTB_CURRENT
+
+#define POT1 K1
+#define POT2 K2
+#define POT3 K3
+
+#endif // _CONFIG_H_
+
---
+++ Baguettes/fwAxl/main.c
@@ -0,0 +1,96 @@
+/*********************************************************************
+ *               analog example for Versa1.0
+ *	Analog capture on connectors K1, K2, K3 and K5. 
+ *********************************************************************/
+
+#define BOARD Versa2
+
+#include <fruit.h>
+#include <analog.h>
+#include <switch.h>
+#include <ADXL345.h>
+#include <i2c_master.h>
+
+t_delay mainDelay;
+ADXL345 adxl1;
+ADXL345 adxl2;
+
+void setup(void) {	
+//----------- Setup ----------------
+	fruitInit();
+			
+//	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
+	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
+
+//----------- Analog setup ----------------
+	analogInit();		// init analog module	
+	analogSelect(0,POT1);
+	analogSelect(1,POT2);
+	analogSelect(2,POT3);
+
+	switchInit();
+	switchSelect(0,SWITCH);
+	switchSelect(1,PEDAL1);
+	switchSelect(2,PEDAL2);
+	switchSelect(3,PEDAL3);
+	switchSelect(4,PEDAL4);
+
+//----------- setup I2C master ----------------
+	i2cm_init(I2C_MASTER, I2C_SLEW_ON, FOSC/400000/4-1);
+	
+//----------- setup ADXL345 ----------------
+	ADXL345Init(&adxl1, 0); // 1st ADXL345's SDO pin is high voltage level
+	ADXL345Init(&adxl2, 1); // 2nd ADXL345's SDO pin is low voltage level
+}
+
+unsigned char cycle = 0;
+
+void loop() {
+// ---------- Main loop ------------
+	fraiseService();	// listen to Fraise events
+	analogService();	// analog management routine
+	switchService();	// 
+	fraiseService();	// listen to Fraise events
+
+	if(delayFinished(mainDelay)) // when mainDelay triggers :
+	{
+		delayStart(mainDelay, 5000); 	// re-init mainDelay
+		if((cycle&1) == 0) ADXL345Send(&adxl1, 1);
+		else ADXL345Send(&adxl2, 2);
+		if(!switchSend()) analogSend();		// send analog channels that changed
+		cycle++;
+		fraiseService();	// listen to Fraise events
+		ADXL345Service(&adxl1);
+		fraiseService();	// listen to Fraise events
+		ADXL345Service(&adxl2);
+	}
+}
+
+// Receiving
+
+void fraiseReceiveChar() // receive text
+{
+	unsigned char c, c2;
+	
+	c=fraiseGetChar();
+	if(c=='L'){		//switch LED on/off 
+		c=fraiseGetChar();
+		digitalWrite(LED, c!='0');		
+	}
+	if(c=='S'){		//switch SWLED on/off 
+		c=fraiseGetChar();
+		digitalWrite(SWLED, c!='0');		
+	}
+	else if(c=='E') { 	// echo text (send it back to host)
+		printf("C");
+		c = fraiseGetLen(); 			// get length of current packet
+		while(c--) printf("%c",fraiseGetChar());// send each received byte
+		putchar('\n');				// end of line
+	}	
+	else if(c=='R') { 	// reset I2C
+		i2cm_init(I2C_MASTER, I2C_SLEW_ON, FOSC/400000/4-1);
+		ADXL345Init(&adxl1, 0);
+		ADXL345Init(&adxl2, 1);
+	}
+}
+
