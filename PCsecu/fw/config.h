3ffb0e283b098f99fd05834624350062ecfb26bd
Antoine Rousseau <_antoine_@metalu.net>
2018-12-20 18:43:25 +0100

    WIP PCsecu

---
+++ PCsecu/fw/config.h
@@ -0,0 +1,36 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define ANALOG_FILTER 4 
+#define ANALOG_THRESHOLD 10
+
+// VNH1:
+#define LEDTEMPO	MA1
+#define LEDWH		MAEN
+#define LEDRED		MAPWM
+//#define 			MOTA_CURRENT	// K3 duplicate
+//#define 			MAEN2			// K2 duplicate
+#define LEDYEL		MA2
+
+// VNH2 (pulled-up):
+#define SWTEMPO		MB1
+#define SWBLUE1		MBEN
+#define SWRED1		MBPWM
+#define SWBLUE2		MOTB_CURRENT
+#define SWRED2		MBEN2
+#define SWELEC1		MB2
+// K11/K12 (pulled-up):
+#define SWELEC2		K11
+#define SWELEC3		K12
+
+#define POTNOEL1	K1
+#define POTNOEL2	K2
+#define POTSECU1	K3
+#define POTSECU2	K4
+#define DIST1		K5
+#define DIST2		K6
+#define ELECTROCUTE	K7
+//#define 			K8
+
+#endif // _CONFIG_H_
+
---
+++ PCsecu/fw/main.c
@@ -0,0 +1,90 @@
+/*********************************************************************
+ *               analog example for Versa1.0
+ *	Analog capture on connectors K1, K2, K3 and K5. 
+ *********************************************************************/
+
+#define BOARD Versa2
+
+#include <fruit.h>
+#include <analog.h>
+#include <switch.h>
+
+t_delay mainDelay;
+
+#define INITLED(led) do{ pinModeDigitalOut(led); digitalSet(led);} while(0)
+#define SETLED(led, on) digitalWrite(led, on == 0)
+void setup(void) {	
+//----------- Setup ----------------
+	fruitInit();
+			
+	
+	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
+	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
+
+	INITLED(LEDTEMPO);
+	INITLED(LEDWH);
+	INITLED(LEDRED);
+	INITLED(LEDYEL);
+
+//----------- Analog setup ----------------
+	analogInit();		// init analog module
+	analogSelect(0,POTNOEL1);	
+	analogSelect(1,POTNOEL2);	
+	analogSelect(2,POTSECU1);	
+	analogSelect(3,POTSECU2);	
+	analogSelect(4,DIST1);	
+	analogSelect(5,DIST2);	
+	analogSelect(6,ELECTROCUTE);	
+
+	switchInit();
+	INTCON2bits.RBPU = 0; // enable pullups on PORTB
+	
+	switchSelect(0,SWTEMPO);
+	switchSelect(1,SWBLUE1);
+	switchSelect(2,SWRED1);
+	switchSelect(3,SWBLUE2);
+	switchSelect(4,SWRED2);
+	switchSelect(5,SWELEC1);
+	switchSelect(6,SWELEC2);
+	switchSelect(7,SWELEC3);
+}
+
+void loop() {
+// ---------- Main loop ------------
+	fraiseService();	// listen to Fraise events
+	analogService();	// analog management routine
+	switchService();	// 
+	if(delayFinished(mainDelay)) // when mainDelay triggers :
+	{
+		delayStart(mainDelay, 5000); 	// re-init mainDelay
+		if(!switchSend()) analogSend();		// send switches channels that changed, if none send analogs that changed
+	}
+}
+
+// Receiving
+
+void fraiseReceiveChar() // receive text
+{
+	unsigned char c,c2;
+	
+	c=fraiseGetChar();
+	if(c=='L'){		//switch LED on/off 
+		c=fraiseGetChar();
+		digitalWrite(LED, c!='0');		
+	}
+	else if(c=='l'){		//switch LED on/off 
+		c=fraiseGetChar();
+		c2=fraiseGetChar();
+		if(c == '0') 		SETLED(LEDTEMPO, c2 != '0');
+		else if(c == '1') 	SETLED(LEDWH, c2 != '0');
+		else if(c == '2') 	SETLED(LEDRED, c2 != '0');
+		else if(c == '3') 	SETLED(LEDYEL, c2 != '0');
+	}
+	else if(c=='E') { 	// echo text (send it back to host)
+		printf("C");
+		c = fraiseGetLen(); 			// get length of current packet
+		while(c--) printf("%c",fraiseGetChar());// send each received byte
+		putchar('\n');				// end of line
+	}	
+}
+
