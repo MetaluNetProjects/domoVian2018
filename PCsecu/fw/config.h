977201d71c4cd99f5207168c59ab3fef5d1ab004
Antoine Rousseau <_antoine_@metalu.net>
2019-01-10 21:18:01 +0000

    PCsecu: fixes and Fraise interface work

--- PCsecu/fw/config.h
+++ PCsecu/fw/config.h
@@ -5,7 +5,7 @@
 #define ANALOG_THRESHOLD 10
 
 // VNH1:
-#define LEDTEMPO	MA1
+//#define LEDTEMPO	MA1
 #define LEDWH		MAEN
 #define LEDRED		MAPWM
 //#define 			MOTA_CURRENT	// K3 duplicate
@@ -13,14 +13,14 @@
 #define LEDYEL		MA2
 
 // VNH2 (pulled-up):
-#define SWTEMPO		MB1
+#define SWELEC2		MB1
 #define SWBLUE1		MBEN
 #define SWRED1		MBPWM
 #define SWBLUE2		MOTB_CURRENT
 #define SWRED2		MBEN2
 #define SWELEC1		MB2
 // K11/K12 (pulled-up):
-#define SWELEC2		K11
+#define SWTEMPO		K11
 #define SWELEC3		K12
 
 #define POTNOEL1	K1
@@ -30,7 +30,7 @@
 #define DIST1		K5
 #define DIST2		K6
 #define ELECTROCUTE	K7
-//#define 			K8
+#define LEDTEMPO 	K8
 
 #endif // _CONFIG_H_
 
